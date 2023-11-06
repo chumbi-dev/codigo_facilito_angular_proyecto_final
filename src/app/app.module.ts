2cfbd5a4ce62c6760d9fc8244c6f6c19812db733
alvarez5127 <142698661+dev-chumbi@users.noreply.github.com>
2023-11-06 10:18:15 -0500

    Version Inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { ListComponent } from './pages/list/list.component';
+import { DetailComponent } from './pages/detail/detail.component';
+
+@NgModule({
+  declarations: [AppComponent, CardComponent, ListComponent, DetailComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
